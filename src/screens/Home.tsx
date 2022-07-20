d7249ecacabbfbe36e08d56c12ac7c0f8537d664
Dario Antonio Ribeiro Junior <darioarjr321@gmail.com>
2022-07-20 09:57:51 -0300

    logout da aplicação funcionando

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
+import  auth  from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
 import Logo from '../assets/logo_secondary.svg';
@@ -30,6 +32,15 @@ export function Home() {
     function handleOpenDetails(orderId: string) {
         navigation.navigate('details', { orderId });
     }
+
+
+    function handleLogout(){
+        auth().signOut().catch(error => {
+            console.log(error);
+            return Alert.alert('Sair', 'Não foi possível sair');
+        });
+    }
+
     return (
         <VStack flex={1} pb={6} bg="gray.700">
             <HStack
@@ -43,6 +54,7 @@ export function Home() {
                 <Logo />
                 <IconButton
                     icon={<SignOut size={26} color={colors.gray[300]} />}
+                    onPress={handleLogout}
                 />
             </HStack>
 
